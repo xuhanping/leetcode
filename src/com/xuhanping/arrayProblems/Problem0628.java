a8d96cdeabc4955f6dc1cd0db682af62902b2f80
xuhanping <xuhanping@AppledeMacBook-Pro.local>
2020-11-07 16:18:20 +0800

    628题完成

---
+++ src/com/xuhanping/arrayProblems/Problem0628.java
@@ -0,0 +1,11 @@
+package com.xuhanping.arrayProblems;
+
+import java.util.Arrays;
+
+public class Problem0628 {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        return Math.max(nums[length-1] * nums[length-2] * nums[length-3], nums[0] * nums[1] * nums[length-1]);
+    }
+}
